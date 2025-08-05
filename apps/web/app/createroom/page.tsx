fd7db79575f63ab19840609ea549ae88872088c8
YuvanX <abhivigneshofficial@gmail.com>
2025-08-05 09:48:04 +0530

    fix: typo

--- apps/web/app/createroom/page.tsx
+++ apps/web/app/createroom/page.tsx
@@ -4,7 +4,6 @@ import { CreateRoomForm } from "@/components/create-room-form";
 
 export default function CreateRoom() {
     return <div className="flex justify-center items-center h-screen">
-     
         <CreateRoomForm />
     </div>
 }
