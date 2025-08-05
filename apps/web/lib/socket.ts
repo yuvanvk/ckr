238fc728cdefc53bab3861da5fde40127af0341d
YuvanX <abhivigneshofficial@gmail.com>
2025-08-05 09:46:25 +0530

    fix: change url

--- apps/web/lib/socket.ts
+++ apps/web/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io, Socket } from "socket.io-client"
 
-const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:8080";
+const URL = process.env.NODE_ENV === "production" ? "https://ckr-ncwi.onrender.com" : "http://localhost:8080";
 
 export const socket: Socket = io(URL)
